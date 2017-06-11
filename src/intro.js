7ec606268a290faba5031c2f7ddfbde6b365c877
Lev Vorobjev <lev.vorobjev@rambler.ru>
2017-06-11 19:05:34 +0300

    Changed coding style whole js files

--- src/intro.js
+++ src/intro.js
@@ -2,13 +2,13 @@
 var isMac = /Mac/.test(navigator.platform);
 
 var shortcuts = {
-  'Cmd-B': toggleBold,
-  'Cmd-I': toggleItalic,
-  'Cmd-K': drawLink,
-  'Cmd-Alt-I': drawImage,
-  "Cmd-'": toggleBlockquote,
-  'Cmd-Alt-L': toggleOrderedList,
-  'Cmd-L': toggleUnOrderedList
+    'Cmd-B': toggleBold,
+    'Cmd-I': toggleItalic,
+    'Cmd-K': drawLink,
+    'Cmd-Alt-I': drawImage,
+    "Cmd-'": toggleBlockquote,
+    'Cmd-Alt-L': toggleOrderedList,
+    'Cmd-L': toggleUnOrderedList
 };
 
 
@@ -16,12 +16,12 @@ var shortcuts = {
  * Fix shortcut. Mac use Command, others use Ctrl.
  */
 function fixShortcut(name) {
-  if (isMac) {
-    name = name.replace('Ctrl', 'Cmd');
-  } else {
-    name = name.replace('Cmd', 'Ctrl');
-  }
-  return name;
+    if (isMac) {
+        name = name.replace('Ctrl', 'Cmd');
+    } else {
+        name = name.replace('Cmd', 'Ctrl');
+    }
+    return name;
 }
 
 
@@ -29,28 +29,28 @@ function fixShortcut(name) {
  * Create icon element for toolbar.
  */
 function createIcon(name, options) {
-  options = options || {};
-  var el = document.createElement('a');
+    options = options || {};
+    var el = document.createElement('a');
 
-  var shortcut = options.shortcut || shortcuts[name];
-  if (shortcut) {
-    shortcut = fixShortcut(shortcut);
-    el.title = shortcut;
-    el.title = el.title.replace('Cmd', '⌘');
-    if (isMac) {
-      el.title = el.title.replace('Alt', '⌥');
+    var shortcut = options.shortcut || shortcuts[name];
+    if (shortcut) {
+        shortcut = fixShortcut(shortcut);
+        el.title = shortcut;
+        el.title = el.title.replace('Cmd', '⌘');
+        if (isMac) {
+            el.title = el.title.replace('Alt', '⌥');
+        }
     }
-  }
 
-  el.className = options.className || 'editor-icon-' + name;
-  return el;
+    el.className = options.className || 'editor-icon-' + name;
+    return el;
 }
 
 function createSep() {
-  el = document.createElement('i');
-  el.className = 'separator';
-  el.innerHTML = '|';
-  return el;
+    el = document.createElement('i');
+    el.className = 'separator';
+    el.innerHTML = '|';
+    return el;
 }
 
 
@@ -58,31 +58,31 @@ function createSep() {
  * The state of CodeMirror at the given position.
  */
 function getState(cm, pos) {
-  pos = pos || cm.getCursor('start');
-  var stat = cm.getTokenAt(pos);
-  if (!stat.type) return {};
+    pos = pos || cm.getCursor('start');
+    var stat = cm.getTokenAt(pos);
+    if (!stat.type) return {};
 
-  var types = stat.type.split(' ');
+    var types = stat.type.split(' ');
 
-  var ret = {}, data, text;
-  for (var i = 0; i < types.length; i++) {
-    data = types[i];
-    if (data === 'strong') {
-      ret.bold = true;
-    } else if (data === 'variable-2') {
-      text = cm.getLine(pos.line);
-      if (/^\s*\d+\.\s/.test(text)) {
-        ret['ordered-list'] = true;
-      } else {
-        ret['unordered-list'] = true;
-      }
-    } else if (data === 'atom') {
-      ret.quote = true;
-    } else if (data === 'em') {
-      ret.italic = true;
+    var ret = {}, data, text;
+    for (var i = 0; i < types.length; i++) {
+        data = types[i];
+        if (data === 'strong') {
+            ret.bold = true;
+        } else if (data === 'variable-2') {
+            text = cm.getLine(pos.line);
+            if (/^\s*\d+\.\s/.test(text)) {
+                ret['ordered-list'] = true;
+            } else {
+                ret['unordered-list'] = true;
+            }
+        } else if (data === 'atom') {
+            ret.quote = true;
+        } else if (data === 'em') {
+            ret.italic = true;
+        }
     }
-  }
-  return ret;
+    return ret;
 }
 
 
@@ -90,34 +90,34 @@ function getState(cm, pos) {
  * Toggle full screen of the editor.
  */
 function toggleFullScreen(editor) {
-  var el = editor.codemirror.getWrapperElement();
+    var el = editor.codemirror.getWrapperElement();
 
-  // https://developer.mozilla.org/en-US/docs/DOM/Using_fullscreen_mode
-  var doc = document;
-  var isFull = doc.fullScreen || doc.mozFullScreen || doc.webkitIsFullScreen;
-  var request = function() {
-    if (el.requestFullScreen) {
-      el.requestFullScreen();
-    } else if (el.mozRequestFullScreen) {
-      el.mozRequestFullScreen();
-    } else if (el.webkitRequestFullScreen) {
-      el.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    // https://developer.mozilla.org/en-US/docs/DOM/Using_fullscreen_mode
+    var doc = document;
+    var isFull = doc.fullScreen || doc.mozFullScreen || doc.webkitIsFullScreen;
+    var request = function() {
+        if (el.requestFullScreen) {
+            el.requestFullScreen();
+        } else if (el.mozRequestFullScreen) {
+            el.mozRequestFullScreen();
+        } else if (el.webkitRequestFullScreen) {
+            el.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    };
+    var cancel = function() {
+        if (doc.cancelFullScreen) {
+            doc.cancelFullScreen();
+        } else if (doc.mozCancelFullScreen) {
+            doc.mozCancelFullScreen();
+        } else if (doc.webkitCancelFullScreen) {
+            doc.webkitCancelFullScreen();
+        }
+    };
+    if (!isFull) {
+        request();
+    } else if (cancel) {
+        cancel();
     }
-  };
-  var cancel = function() {
-    if (doc.cancelFullScreen) {
-      doc.cancelFullScreen();
-    } else if (doc.mozCancelFullScreen) {
-      doc.mozCancelFullScreen();
-    } else if (doc.webkitCancelFullScreen) {
-      doc.webkitCancelFullScreen();
-    }
-  };
-  if (!isFull) {
-    request();
-  } else if (cancel) {
-    cancel();
-  }
 }
 
 
@@ -125,7 +125,7 @@ function toggleFullScreen(editor) {
  * Action for toggling bold.
  */
 function toggleBold(editor) {
-  _toggleBlock(editor, 'bold', '**');
+    _toggleBlock(editor, 'bold', '**');
 }
 
 
@@ -133,22 +133,22 @@ function toggleBold(editor) {
  * Action for toggling italic.
  */
 function toggleItalic(editor) {
-  _toggleBlock(editor, 'italic', '*');
+    _toggleBlock(editor, 'italic', '*');
 }
 
 /**
  * Action for toggling code block.
  */
 function toggleCodeBlock(editor) {
-  _toggleBlock(editor, 'code', '```\r\n', '\r\n```');
+    _toggleBlock(editor, 'code', '```\r\n', '\r\n```');
 }
 
 /**
  * Action for toggling blockquote.
  */
 function toggleBlockquote(editor) {
-  var cm = editor.codemirror;
-  _toggleLine(cm, 'quote');
+    var cm = editor.codemirror;
+    _toggleLine(cm, 'quote');
 }
 
 
@@ -157,8 +157,8 @@ function toggleBlockquote(editor) {
  * Action for toggling ul.
  */
 function toggleUnOrderedList(editor) {
-  var cm = editor.codemirror;
-  _toggleLine(cm, 'unordered-list');
+    var cm = editor.codemirror;
+    _toggleLine(cm, 'unordered-list');
 }
 
 
@@ -166,8 +166,8 @@ function toggleUnOrderedList(editor) {
  * Action for toggling ol.
  */
 function toggleOrderedList(editor) {
-  var cm = editor.codemirror;
-  _toggleLine(cm, 'ordered-list');
+    var cm = editor.codemirror;
+    _toggleLine(cm, 'ordered-list');
 }
 
 
@@ -175,9 +175,9 @@ function toggleOrderedList(editor) {
  * Action for drawing a link.
  */
 function drawLink(editor) {
-  var cm = editor.codemirror;
-  var stat = getState(cm);
-  _replaceSelection(cm, stat.link, '[', '](http://)');
+    var cm = editor.codemirror;
+    var stat = getState(cm);
+    _replaceSelection(cm, stat.link, '[', '](http://)');
 }
 
 
@@ -185,9 +185,9 @@ function drawLink(editor) {
  * Action for drawing an img.
  */
 function drawImage(editor) {
-  var cm = editor.codemirror;
-  var stat = getState(cm);
-  _replaceSelection(cm, stat.image, '![', '](http://)');
+    var cm = editor.codemirror;
+    var stat = getState(cm);
+    _replaceSelection(cm, stat.image, '![', '](http://)');
 }
 
 
@@ -195,9 +195,9 @@ function drawImage(editor) {
  * Undo action.
  */
 function undo(editor) {
-  var cm = editor.codemirror;
-  cm.undo();
-  cm.focus();
+    var cm = editor.codemirror;
+    cm.undo();
+    cm.focus();
 }
 
 
@@ -205,137 +205,137 @@ function undo(editor) {
  * Redo action.
  */
 function redo(editor) {
-  var cm = editor.codemirror;
-  cm.redo();
-  cm.focus();
+    var cm = editor.codemirror;
+    cm.redo();
+    cm.focus();
 }
 
 /**
  * Preview action.
  */
 function togglePreview(editor) {
-  var toolbar = editor.toolbar.preview;
-  var parse = editor.constructor.markdown;
-  var cm = editor.codemirror;
-  var wrapper = cm.getWrapperElement();
-  var preview = wrapper.lastChild;
-  if (!/editor-preview/.test(preview.className)) {
-    preview = document.createElement('div');
-    preview.className = 'editor-preview';
-    wrapper.appendChild(preview);
-  }
-  if (/editor-preview-active/.test(preview.className)) {
-    preview.className = preview.className.replace(
-      /\s*editor-preview-active\s*/g, ''
-    );
-    toolbar.className = toolbar.className.replace(/\s*active\s*/g, '');
-  } else {
-    /* When the preview button is clicked for the first time,
-     * give some time for the transition from editor.css to fire and the view to slide from right to left,
-     * instead of just appearing.
-     */
-    setTimeout(function() {preview.className += ' editor-preview-active'}, 1);
-    toolbar.className += ' active';
-  }
-  var text = cm.getValue();
-  preview.innerHTML = parse(text);
+    var toolbar = editor.toolbar.preview;
+    var parse = editor.constructor.markdown;
+    var cm = editor.codemirror;
+    var wrapper = cm.getWrapperElement();
+    var preview = wrapper.lastChild;
+    if (!/editor-preview/.test(preview.className)) {
+        preview = document.createElement('div');
+        preview.className = 'editor-preview';
+        wrapper.appendChild(preview);
+    }
+    if (/editor-preview-active/.test(preview.className)) {
+        preview.className = preview.className.replace(
+            /\s*editor-preview-active\s*/g, ''
+        );
+        toolbar.className = toolbar.className.replace(/\s*active\s*/g, '');
+    } else {
+        /* When the preview button is clicked for the first time,
+         * give some time for the transition from editor.css to fire and the view to slide from right to left,
+         * instead of just appearing.
+         */
+        setTimeout(function() {preview.className += ' editor-preview-active'}, 1);
+        toolbar.className += ' active';
+    }
+    var text = cm.getValue();
+    preview.innerHTML = parse(text);
 }
 
 function _replaceSelection(cm, active, start, end) {
-  var text;
-  var startPoint = cm.getCursor('start');
-  var endPoint = cm.getCursor('end');
-  if (active) {
-    text = cm.getLine(startPoint.line);
-    start = text.slice(0, startPoint.ch);
-    end = text.slice(startPoint.ch);
-    cm.setLine(startPoint.line, start + end);
-  } else {
-    text = cm.getSelection();
-    cm.replaceSelection(start + text + end);
+    var text;
+    var startPoint = cm.getCursor('start');
+    var endPoint = cm.getCursor('end');
+    if (active) {
+        text = cm.getLine(startPoint.line);
+        start = text.slice(0, startPoint.ch);
+        end = text.slice(startPoint.ch);
+        cm.setLine(startPoint.line, start + end);
+    } else {
+        text = cm.getSelection();
+        cm.replaceSelection(start + text + end);
 
-    startPoint.ch += start.length;
-    endPoint.ch += start.length;
-  }
-  cm.setSelection(startPoint, endPoint);
-  cm.focus();
+        startPoint.ch += start.length;
+        endPoint.ch += start.length;
+    }
+    cm.setSelection(startPoint, endPoint);
+    cm.focus();
 }
 
 
 function _toggleLine(cm, name) {
-  var stat = getState(cm);
-  var startPoint = cm.getCursor('start');
-  var endPoint = cm.getCursor('end');
-  var repl = {
-    quote: /^(\s*)\>\s+/,
-    'unordered-list': /^(\s*)(\*|\-|\+)\s+/,
-    'ordered-list': /^(\s*)\d+\.\s+/
-  };
-  var map = {
-    quote: '> ',
-    'unordered-list': '* ',
-    'ordered-list': '1. '
-  };
-  for (var i = startPoint.line; i <= endPoint.line; i++) {
-    (function(i) {
-      var text = cm.getLine(i);
-      if (stat[name]) {
-        text = text.replace(repl[name], '$1');
-      } else {
-        text = map[name] + text;
-      }
-      cm.setLine(i, text);
-    })(i);
-  }
-  cm.focus();
+    var stat = getState(cm);
+    var startPoint = cm.getCursor('start');
+    var endPoint = cm.getCursor('end');
+    var repl = {
+        quote: /^(\s*)\>\s+/,
+        'unordered-list': /^(\s*)(\*|\-|\+)\s+/,
+        'ordered-list': /^(\s*)\d+\.\s+/
+    };
+    var map = {
+        quote: '> ',
+        'unordered-list': '* ',
+        'ordered-list': '1. '
+    };
+    for (var i = startPoint.line; i <= endPoint.line; i++) {
+        (function(i) {
+            var text = cm.getLine(i);
+            if (stat[name]) {
+                text = text.replace(repl[name], '$1');
+            } else {
+                text = map[name] + text;
+            }
+            cm.setLine(i, text);
+        })(i);
+    }
+    cm.focus();
 }
 
 function _toggleBlock(editor, type, start_chars, end_chars){
-  end_chars = (typeof end_chars === 'undefined') ? start_chars : end_chars;
-  var cm = editor.codemirror;
-  var stat = getState(cm);
+    end_chars = (typeof end_chars === 'undefined') ? start_chars : end_chars;
+    var cm = editor.codemirror;
+    var stat = getState(cm);
 
-  var text;
-  var start = start_chars;
-  var end = end_chars;
+    var text;
+    var start = start_chars;
+    var end = end_chars;
 
-  var startPoint = cm.getCursor('start');
-  var endPoint = cm.getCursor('end');
-  if (stat[type]) {
-    text = cm.getLine(startPoint.line);
-    start = text.slice(0, startPoint.ch);
-    end = text.slice(startPoint.ch);
-    startRegex = new RegExp("/^(.*)?(\*|\_){" + start_chars.length + "}(\S+.*)?$/", "g")
-    start = start.replace(startRegex, '$1$3');
-    endRegex = new RegExp("/^(.*\S+)?(\*|\_){" + end_chars.length + "}(\s+.*)?$/", "g")
-    end = end.replace(endRegex, '$1$3');
-    startPoint.ch -= start_chars.length;
-    endPoint.ch -= end_chars.length;
-    cm.setLine(startPoint.line, start + end);
-  } else {
-    text = cm.getSelection();
-    cm.replaceSelection(start + text + end);
+    var startPoint = cm.getCursor('start');
+    var endPoint = cm.getCursor('end');
+    if (stat[type]) {
+        text = cm.getLine(startPoint.line);
+        start = text.slice(0, startPoint.ch);
+        end = text.slice(startPoint.ch);
+        startRegex = new RegExp("/^(.*)?(\*|\_){" + start_chars.length + "}(\S+.*)?$/", "g")
+        start = start.replace(startRegex, '$1$3');
+        endRegex = new RegExp("/^(.*\S+)?(\*|\_){" + end_chars.length + "}(\s+.*)?$/", "g")
+        end = end.replace(endRegex, '$1$3');
+        startPoint.ch -= start_chars.length;
+        endPoint.ch -= end_chars.length;
+        cm.setLine(startPoint.line, start + end);
+    } else {
+        text = cm.getSelection();
+        cm.replaceSelection(start + text + end);
 
-    startPoint.ch += start_chars.length;
-    endPoint.ch += end_chars.length;
-  }
-  cm.setSelection(startPoint, endPoint);
-  cm.focus();
+        startPoint.ch += start_chars.length;
+        endPoint.ch += end_chars.length;
+    }
+    cm.setSelection(startPoint, endPoint);
+    cm.focus();
 }
 
 
 /* The right word count in respect for CJK. */
 function wordCount(data) {
-  var pattern = /[a-zA-Z0-9_\u0392-\u03c9]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/g;
-  var m = data.match(pattern);
-  var count = 0;
-  if( m === null ) return count;
-  for (var i = 0; i < m.length; i++) {
-    if (m[i].charCodeAt(0) >= 0x4E00) {
-      count += m[i].length;
-    } else {
-      count += 1;
+    var pattern = /[a-zA-Z0-9_\u0392-\u03c9]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/g;
+    var m = data.match(pattern);
+    var count = 0;
+    if( m === null ) return count;
+    for (var i = 0; i < m.length; i++) {
+        if (m[i].charCodeAt(0) >= 0x4E00) {
+            count += m[i].length;
+        } else {
+            count += 1;
+        }
     }
-  }
-  return count;
+    return count;
 }
--- vendor/continuelist.js
+++ vendor/continuelist.js
@@ -1,62 +1,62 @@
 const listRE = /^(\s*)([*+-]|(\d+)\.)([\w+(\s+\w+)]|[\s*])/,
-    emptyListRE = /^(\s*)([*+-]|(\d+)\.)(\s*)$/,
-    unorderedBullets = '*+-';
+        emptyListRE = /^(\s*)([*+-]|(\d+)\.)(\s*)$/,
+        unorderedBullets = '*+-';
 
 var inListState = function(cm, pos){
-  return cm.getStateAfter(pos.line).list || null;
+    return cm.getStateAfter(pos.line).list || null;
 };
 
 var inListOrNot = function(cm){
-  var pos = cm.getCursor();
-  return inListState(cm, pos);
+    var pos = cm.getCursor();
+    return inListState(cm, pos);
 };
 
 CodeMirror.commands.shiftTabAndIndentContinueMarkdownList = function(cm){
-  var inList = inListOrNot(cm);
+    var inList = inListOrNot(cm);
 
-  if(inList !== null){
-    cm.execCommand('insertTab');
-    return;
-  }
+    if(inList !== null){
+        cm.execCommand('insertTab');
+        return;
+    }
 
-  cm.execCommand('indentLess');
+    cm.execCommand('indentLess');
 };
 
 CodeMirror.commands.tabAndIndentContinueMarkdownList = function(cm){
-  var inList = inListOrNot(cm);
+    var inList = inListOrNot(cm);
 
-  if(inList !== null){
-    cm.execCommand('insertTab');
-    return;
-  }
+    if(inList !== null){
+        cm.execCommand('insertTab');
+        return;
+    }
 
-  cm.execCommand('indentMore');
+    cm.execCommand('indentMore');
 };
 
 CodeMirror.commands.newlineAndIndentContinueMarkdownList = function(cm){
-  var pos, tok, match, emptyMatch, inList;
+    var pos, tok, match, emptyMatch, inList;
 
-  pos = cm.getCursor();
-  tok = cm.getTokenAt(pos);
-  emptyMatch = cm.getLine(pos.line).match(emptyListRE);
-  inList = inListState(cm, pos);
+    pos = cm.getCursor();
+    tok = cm.getTokenAt(pos);
+    emptyMatch = cm.getLine(pos.line).match(emptyListRE);
+    inList = inListState(cm, pos);
 
-  if (!inList && emptyMatch){
-    cm.replaceRange("", {line: pos.line , ch:tok.start}, {line:pos.line , ch:tok.end});
-    cm.execCommand('delLineLeft');
-    cm.execCommand('newlineAndIndent');
-    return;
-  }
+    if (!inList && emptyMatch){
+        cm.replaceRange("", {line: pos.line , ch:tok.start}, {line:pos.line , ch:tok.end});
+        cm.execCommand('delLineLeft');
+        cm.execCommand('newlineAndIndent');
+        return;
+    }
 
-  if (!inList || !(match = cm.getLine(pos.line).match(listRE))) {
-    cm.execCommand('newlineAndIndent');
-    return;
-  }
+    if (!inList || !(match = cm.getLine(pos.line).match(listRE))) {
+        cm.execCommand('newlineAndIndent');
+        return;
+    }
 
-  var indent = match[1], after = " ";
-  var bullet = unorderedBullets.indexOf(match[2]) >= 0
-      ? match[2]
-      : (parseInt(match[3], 10) + 1) + '.';
+    var indent = match[1], after = " ";
+    var bullet = unorderedBullets.indexOf(match[2]) >= 0
+            ? match[2]
+            : (parseInt(match[3], 10) + 1) + '.';
 
-  cm.replaceSelection('\n' + indent + bullet + after, 'end');
+    cm.replaceSelection('\n' + indent + bullet + after, 'end');
 };
