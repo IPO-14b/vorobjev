b8de8fb0dfbc6a6ebd62ca48d5d26b9ff6cae889
Lev Vorobjev <lev.vorobjev@rambler.ru>
2017-06-12 06:07:55 +0300

    Tabulation fix #3

--- vendor/icomoon/lte-ie7.js
+++ vendor/icomoon/lte-ie7.js
@@ -6,26 +6,26 @@ window.onload = function() {
         el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
     }
     var icons = {
-            'icon-bold' : '&#xe000;',
-            'icon-italic' : '&#xe001;',
-            'icon-quotes-left' : '&#xe003;',
-            'icon-list' : '&#xe004;',
-            'icon-numbered-list' : '&#xe005;',
-            'icon-link' : '&#xe006;',
-            'icon-image' : '&#xe007;',
-            'icon-play' : '&#xe008;',
-            'icon-music' : '&#xe009;',
-            'icon-contract' : '&#xe00a;',
-            'icon-expand' : '&#xe00b;',
-            'icon-question' : '&#xe00c;',
-            'icon-info' : '&#xe00d;',
-            'icon-undo' : '&#xe00e;',
-            'icon-redo' : '&#xe00f;',
-            'icon-code' : '&#xe011;',
-            'icon-eye' : '&#xe002;'
-        },
-        els = document.getElementsByTagName('*'),
-        i, attr, html, c, el;
+        'icon-bold' : '&#xe000;',
+        'icon-italic' : '&#xe001;',
+        'icon-quotes-left' : '&#xe003;',
+        'icon-list' : '&#xe004;',
+        'icon-numbered-list' : '&#xe005;',
+        'icon-link' : '&#xe006;',
+        'icon-image' : '&#xe007;',
+        'icon-play' : '&#xe008;',
+        'icon-music' : '&#xe009;',
+        'icon-contract' : '&#xe00a;',
+        'icon-expand' : '&#xe00b;',
+        'icon-question' : '&#xe00c;',
+        'icon-info' : '&#xe00d;',
+        'icon-undo' : '&#xe00e;',
+        'icon-redo' : '&#xe00f;',
+        'icon-code' : '&#xe011;',
+        'icon-eye' : '&#xe002;'
+    },
+    els = document.getElementsByTagName('*'),
+    i, attr, html, c, el;
     for (i = 0; i < els.length; i += 1) {
         el = els[i];
         attr = el.getAttribute('data-icon');
